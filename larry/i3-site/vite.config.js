8bba46d3713108c0a967cec5b0ced52d9ba2d027
Brian Kelleher <brian.kelleher@uconn.edu>
2025-07-09 11:11:20 -0400

    vite manifest

--- larry/i3-site/vite.config.js
+++ larry/i3-site/vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: [
+                'resources/js/app.js',
+                'resources/js/circleTurnAnimation.js',
+                'resources/js/coffeeAnimation.js',
+                'resources/js/explodingPhrases.js',
+                'resources/js/photoScroller.js',
+                'resources/js/starParticles.js',
+        ],
             refresh: true,
         })
     ],
