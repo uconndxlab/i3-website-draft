f0ed23708be7fe5aa8a64bba2d54c4a9d064f0df
Brian Kelleher <brian.kelleher@uconn.edu>
2025-07-13 22:10:55 -0400

    update bootstrap icons for beaker icon

--- larry/i3-site/resources/views/layouts/app.blade.php
+++ larry/i3-site/resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <!-- Bootstrap Icons (optional) -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
         rel="stylesheet">
