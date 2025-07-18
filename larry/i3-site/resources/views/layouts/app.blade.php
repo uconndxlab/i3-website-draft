8cfb93630ab13baa7648ae703558baf25675c32b
Joel Salisbury <joel.salisbury@gmail.com>
2025-07-18 11:34:02 -0400

    copy tweak footeR

--- larry/i3-site/resources/views/layouts/app.blade.php
+++ larry/i3-site/resources/views/layouts/app.blade.php
@@ -133,7 +133,7 @@
                             <span class="text-primary">i3</span> Internal Insights & Innovation
                         </h5>
                         <p class="text-light opacity-75 mb-3">
-                            We're a team of student developers, designers, and researchers working to create innovative digital solutions for the UConn community.
+                            We're a team of developers, designers, and researchers working to create innovative digital solutions for the UConn community.
                         </p>
                         <div class="d-flex gap-3">
                             <a href="https://github.com/uconndxlab" target="_blank" class="text-light opacity-75 hover-opacity-100 fs-5">
@@ -218,7 +218,8 @@
                     </div>
                     <div class="col-md-6 text-md-end">
                         <p class="text-light opacity-75 mb-0 small">
-                            Built with ❤️ and Laravel by the i3 team
+                            Built with ❤️ and Laravel by the <acronym style="cursor: help"
+                            title="Victoria, BK, Natalie, Shium, and Joel">i3 team</acronym>
                         </p>
                     </div>
                 </div>
